1328b101bb0b1f5c152a4e42d7936f27f46a875d
Anatol Belski <anbelski@linux.microsoft.com>
2021-09-30 02:35:17 +0200

    tool: More for the base structure
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 
 from setuptools import setup
 
+import vmimg
+
 def long_desc():
     with open("README.md", "r", encoding="utf-8") as f:
         return f.read()
 
 setup(name="vmimg",
-      version="0.0.1",
+      version=vmimg.__version__,
       description="VM disk image utils",
       long_description=long_desc(),
       long_description_content_type="text/markdown",
