e31067a6311fba60d321e15de300d2f0d1174951
Anatol Belski <anbelski@linux.microsoft.com>
2021-10-05 17:41:13 +0200

    part: Remove unused function
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- src/vmimg/disk/part.py
+++ src/vmimg/disk/part.py
@@ -3,12 +3,6 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def pt(l, d):
-    u = d + 1
-    if len(idx) <= u:
-        return l[idx[d]:].strip()
-    return l[idx[d]:idx[u]].strip()
-
 
 class Part():
     def __init__(self, props):
